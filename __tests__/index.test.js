06135cd4329a6273b44c888d8d0c52ffe8e97c71
Cody Brunner <rockchalkwushock@icloud.com>
2017-10-14 07:17:30 -0500

    fix(root): Placed __tests__ in root :neutral_face:
    __tests__/ was populating in the build causing conflicts with `now`

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { shallow, render } from 'enzyme'
 
-import Index from '../'
+import Index from '../pages'
 
 describe('Index Page', () => {
   test('shallow', () => {
