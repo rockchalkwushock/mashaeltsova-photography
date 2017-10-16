1d8c91f87bd8b5cb79f57a8873e237b66f03fe47
Cody Brunner <rockchalkwushock@icloud.com>
2017-10-16 14:30:16 -0500

    fix(BookingForm): Required message input since it's required server side
    Updated i18n for this fix as well

--- components/BookingForm/validate.js
+++ components/BookingForm/validate.js
@@ -3,6 +3,7 @@ import { messages } from '../../lib'
 const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
 // @see https://stackoverflow.com/a/13353676/6520579
 const nameRegex = /^[A-Za-z\s]+$/
+const messageRegex = /^[A-Za-z0-9.!?,;\s]+$/
 // @see https://stackoverflow.com/a/18029630/6520579
 const phoneRegex = /\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\W*\d\W*\d\W*\d\W*\d\W*\d\W*\d\W*\d\W*\d\W*(\d{1,2})$/
 
@@ -45,6 +46,16 @@ export default (values, { intl }) => {
     errors.phone = intl.formatMessage(messages.errPhoneReq)
   } else if (!phoneRegex.test(values.phone)) {
     errors.phone = intl.formatMessage(messages.errPhoneVal)
+  }
+
+  if (!values.message) {
+    errors.message = intl.formatMessage(messages.errMessageReq)
+  } else if (values.message.length < 10) {
+    errors.message = intl.formatMessage(messages.errMessageMinLen)
+  } else if (values.message.length > 140) {
+    errors.message = intl.formatMessage(messages.errMessageMaxLen)
+  } else if (!messageRegex.test(values.message)) {
+    errors.message = intl.formatMessage(messages.errMessageVal)
   }
 
   return errors
