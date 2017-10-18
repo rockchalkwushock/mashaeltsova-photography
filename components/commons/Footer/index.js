fa4668655195347a73dcc28387d99f76e2646b7b
Cody Brunner <rockchalkwushock@icloud.com>
2017-10-18 08:13:55 -0500

    feat(Footer): Initial stylings for Footer added
    i18n added as well

--- components/commons/Footer/index.js
+++ components/commons/Footer/index.js
@@ -1,16 +1,50 @@
+import Icon from './Icon'
+
+import { styles } from '../../../lib'
+
 /**
- * REVIEW
  * FIXME
- * I will need to pass props on this like `intl`
+ *
+ * Add links to social icons.
  */
-const Footer = ({ children }) => (
+
+const Footer = ({ intl }) => (
   <footer>
-    {children}
+    <div className="container social">
+      <Icon icon="facebook" size="2x" url="#" />
+      <Icon icon="instagram" size="2x" url="#" />
+      <Icon icon="vk" size="2x" url="#" />
+      <Icon icon="twitter" size="2x" url="#" />
+    </div>
+    <div className="container copyright">
+      <Icon icon="copyright" intl={intl} url="https://rcws-development.com" />
+    </div>
     <style>{`
     footer {
-      background-color: lightcoral;
+      background-color: ${styles.colors.peach};
+      display: grid;
       grid-column: span 7;
-      height: 50px;
+      grid-gap: 0.5em;
+      grid-template-columns: 1fr;
+      padding: 0.5em 0.75em;
+    }
+    .container {
+      text-align: center;
+    }
+    @media (min-width: 768px) {
+      footer {
+        grid-template-columns: repeat(2, 1fr);
+      }
+      .copyright,
+      .social {
+        align-self: center;
+      }
+      .social {
+        justify-self: start;
+      }
+      .copyright {
+        justify-self: end;
+      }
     }
   `}</style>
   </footer>
