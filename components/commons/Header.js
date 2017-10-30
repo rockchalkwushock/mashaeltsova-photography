51e66465f8791dadf81f65f841835861fbfef016
Cody Brunner <rockchalkwushock@icloud.com>
2017-10-30 18:13:21 -0500

    added meta data, made some notes

--- components/commons/Header.js
+++ components/commons/Header.js
@@ -1,5 +1,17 @@
 import { styles } from '../../lib'
 
+/**
+ * TODO FIXME
+ *
+ * Will want to render a different view for header when at
+ * `/gallery` vs `/` because the gallery will already be heavy
+ * with all the images being loaded.
+ *
+ * Pass `pathname` on the header and then run and if/else.
+ * Possibly render a banner with the logo in one corner and
+ * 'Masha Eltsova Photography' spanning the rest in Courgette or Nunito.
+ */
+
 const Header = ({ children }) => (
   <header>
     {children}
--- components/commons/Meta.js
+++ components/commons/Meta.js
@@ -1,18 +1,69 @@
 import Head from 'next/head'
 import { styles } from '../../lib'
 
+/**
+ * TODO
+ * 1. Add Google Site Verification <meta name="google-site-verification" content="getTokenFromGoogle" />
+ * 2. Add /static/ & Favicon icons.
+ * 3. Add Facebook Page Insights <meta name="fb:admins" content="fbUserID" />
+ * 4. Pass props.message to access the localeData for specific keys.
+ * 5. Finish Filling in empty keys.
+ */
+
 export default () => (
   <div>
     <Head>
+      {/* COMMON TAGS */}
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="X-UA-Compatible" content="ie=edge,chrome=1" />
       <title>Masha Eltsova Photography</title>
+      {/* SEO */}
       <meta name="author" content="RCWS Development" />
       <meta
         name="description"
         content="Masha Eltsova is a professional photographer in Moscow, Russia who offers <options>. Schedule a shoot with her today!"
       />
+      {/* SCHEMA.ORG FOR GOOGLE */}
+      <meta itemProp="name" content="Masha Eltsova Photography" />
+      <meta
+        itemProp="description"
+        content="Masha Eltsova is a professional photographer in Moscow, Russia who offers <options>. Schedule a shoot with her today!"
+      />
+      <meta itemProp="image" content="" />
+      {/* TWITTER */}
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content="Masha Eltsova Photography" />
+      <meta name="twitter:site" content="@user" />
+      <meta name="twitter:creator" content="@user" />
+      <meta
+        name="twitter:description"
+        content="Masha Eltsova is a professional photographer in Moscow, Russia who offers <options>. Schedule a shoot with her today!"
+      />
+      <meta name="twitter:image" content="" />
+      <meta name="twitter:image:alt" content="Masha Eltsova Photography" />
+      <meta name="twitter:url" content="https://mashaeltsovaphotography.com" />
+      {/* FACEBOOK, PINTREST, GOOGLE+ */}
+      <meta name="og:title" content="Masha Eltsova Photography" />
+      <meta
+        name="og:description"
+        content="Masha Eltsova is a professional photographer in Moscow, Russia who offers <options>. Schedule a shoot with her today!"
+      />
+      <meta name="og:image" content="" />
+      <meta name="og:image:secure_url" content="" />
+      <meta name="og:image:alt" content="Masha Eltsova Photography" />
+      <meta name="og:url" content="https://mashaeltsovaphotography.com" />
+      <meta name="og:site_name" content="Masha Eltsova Photography" />
+      <meta name="og:locale" content="ru_RU" />
+      <meta name="og:locale:alternate" content="en_US" />
+      <meta name="og:locale:alternate" content="en_GB" />
+      <meta name="og:type" content="website" />
+      {/* SPIDERS & BOTS */}
+      <meta name="robots" content="index,follow,archive" />
+      <meta name="googlebot" content="all" />
+      {/* GOOGLE SITE VERIFICATION */}
+      {/* FACEBOOK PAGE ANALYTICS */}
+      {/* FAVICONS & TOUCH ICONS */}
       {/* EXTERNAL STYLESHEETS */}
       {/* Normalize CDN */}
       <link
