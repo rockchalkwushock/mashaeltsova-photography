8984423752cb987745247bcfb038a63d45ce7306
Cody Brunner <rockchalkwushock@icloud.com>
2017-11-05 12:14:08 -0600

    removed test components

--- _components/Modal.js
+++
@@ -1,9 +0,0 @@
-import Photo from './Photo'
-
-const Modal = ({ id, onDismiss }) => (
-  <div onClick={onDismiss}>
-    <Photo id={id} />
-  </div>
-)
-
-export default Modal
--- _components/index.js
+++
@@ -1,5 +0,0 @@
-import Grid from './Grid'
-import Modal from './Modal'
-import Thumb from './Thumb'
-
-export { Grid, Modal, Thumb }
