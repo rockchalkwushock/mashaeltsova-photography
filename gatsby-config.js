cbe40742f0523564cd31980099608b91d712563c
Cody Brunner <rockchalkwushock@icloud.com>
2018-03-21 08:18:13 -0500

    feat(adds images): adds gallery page creation by category [skip ci]

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,20 @@
+const { resolve } = require('path')
+
 module.exports = {
   siteMetadata: {
     title: 'Masha Eltsova Photography'
   },
-  plugins: ['gatsby-plugin-react-next', 'gatsby-plugin-styled-components']
+  plugins: [
+    'gatsby-plugin-react-next',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: resolve(__dirname, 'images')
+      }
+    },
+    'gatsby-transformer-sharp'
+  ]
 }
---
+++ src/templates/gallery.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const GalleryTemplate = () => <h1>Hello</h1>
+
+export default GalleryTemplate
